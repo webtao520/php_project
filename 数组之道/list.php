f70bf04abe63b1f1dfc4c20c55ed0331c409afa0
Geoffrey <1183681473@qq.com>
2020-12-07 13:32:11 +0800

    list

---
+++ 数组之道/list.php
@@ -0,0 +1,10 @@
+<?php
+//把数组中的值赋给一些变量：
+$my_array = array("Dog","Cat","Horse");
+list($a,$b,$c)=$my_array;
+echo "I have several aniamlis,a $a, a $b and a $c.";
+
+/**
+PS D:\phpstudy_pro\www\php_project\数组之道> php .\list.php
+I have several aniamlis,a Dog, a Cat and a Horse.
+ */
