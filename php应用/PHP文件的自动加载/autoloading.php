41faa2b75b66d6d5bfa814f9d4103af4ab6e013d
Geoffrey <1183681473@qq.com>
2020-12-10 10:18:44 +0800

    自动加载

--- php应用/PHP文件的自动加载/autoloading.php
+++ php应用/PHP文件的自动加载/autoloading.php
@@ -78,3 +78,14 @@
 
  */
 
+spl_autoload_register(function ($name) {
+    echo "Want to load $name.\n";
+    throw new Exception("Unable to load $name.");
+});
+
+try {
+    $obj = new NonLoadableClass();
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+
